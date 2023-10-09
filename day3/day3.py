77d6ef1a6ce57cdd2008d5e3de3eb466ff81f178
Odin Grav <91graodi@stud.akademiet.no>
2023-10-09 12:43:53 +0200

    finished day 3

--- day3/day3.py
+++ day3/day3.py
@@ -3,18 +3,86 @@ with open('data.txt') as file:
 
 items = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-totalPriority = 0
-for rucksack in rucksacks:
-    compartment1 = rucksack[:len(rucksack) // 2]
-    compartment2 = rucksack[len(rucksack) // 2:]
+def findPriorityOfItems(items):
+    priorityOfItems = 0
+    for item in items:
+        priorityOfItems += findPriorityOfItem(item)
     
+    return priorityOfItems
+
+def findPriorityOfItem(item):
+    possibleItems = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    item = possibleItems.index(item) + 1
+    
+    return item
+
+
+def findMisplacedItems(rucksacks):
+    misplacedItems = []
+    for rucksack in rucksacks:
+        misplacedItems.append(findMisplacedItem(rucksack))
+
+    return misplacedItems
+
+
+def findMisplacedItem(rucksack):
     misplacedItem = ''
 
-    for item in compartment1:
-        if item in compartment2:
-            misplacedItem = item
+    compartment1 = rucksack[:len(rucksack) // 2]
+    compartment2 = rucksack[len(rucksack) // 2:]
 
-    priorityOfItem = items.index(misplacedItem) + 1
-    totalPriority += priorityOfItem
+    misplacedItem = findMathingItemsInTwoArrays(compartment1, compartment2)[0]
 
-print(totalPriority)
+    return misplacedItem
+
+def findMathingItemsInTwoArrays(array1, arrray2):
+    matchingItems = []
+
+    for item in array1:
+        if item in arrray2:
+            matchingItems.append(item)
+    
+    return matchingItems
+
+def findMatchingItemsInMultipleArrays(arrays):
+    matchingItem = ''
+    potentialMatchingItems = arrays[0]
+
+    for array in arrays:
+        potentialMatchingItems = findMathingItemsInTwoArrays(potentialMatchingItems, array)
+
+    matchingItem = potentialMatchingItems[0]
+
+    return matchingItem
+
+def findGroupBadges(rucksacks):
+    groupBadges = []
+    for rucksack in rucksacks[::3]:
+        group = rucksacks[rucksacks.index(rucksack):rucksacks.index(rucksack) + 3]
+        groupBadges.append(findGroupBadge(group))
+
+    return groupBadges
+
+    
+def findGroupBadge(group):
+    groupBadge = ''
+    potentialgroupeBadge = group[0]
+
+    for rucksack in group:
+        potentialgroupeBadge = findMathingItemsInTwoArrays(potentialgroupeBadge, rucksack)
+
+    groupBadge = potentialgroupeBadge[0]
+
+    return groupBadge
+
+
+misplacedItems = findMisplacedItems(rucksacks)
+priorityOfMisplacedItems = findPriorityOfItems(misplacedItems)
+
+groupBadges = findGroupBadges(rucksacks)
+priorityofGroupBadges = findPriorityOfItems(groupBadges)
+
+
+
+print(f'Missplaced items: {priorityOfMisplacedItems}')
+print(f'Group badges: {priorityofGroupBadges}')
